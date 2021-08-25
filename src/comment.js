d02e522a4b79ecc3b5d3910c62ca1e4030d53073
Kieryn Moore <kierynmoore@hotmail.com>
2021-08-25 11:27:18 +0930

    the bell button now changes toggles blue on and off if any comments are unseen

--- src/comment.js
+++ src/comment.js
@@ -1,10 +1,19 @@
 import React from 'react';
 
-
-
 export function Comment(props) {
-  const {id, body, acknowledged, dates: {created: {date_time}}, user: {first_name, last_name, image: {square_url}}} = props;
-  const [isSeen, setIsSeen] = React.useState(acknowledged);
+  const {commentPosition, commentsData, setCommentsData} = props;
+  let comment = commentsData.find(obj => obj.id === commentPosition);
+  const {id, body, acknowledged, dates: {created: {date_time}}, user: {first_name, last_name, image: {square_url}}} = comment;
+
+  // {/* Function changes comment state from false to true */}
+  const changeAcknowledged = (comment_id) => {
+    let clonedData = [...commentsData];
+    let objIndex = clonedData.findIndex((obj => obj.id == comment_id));
+    clonedData[objIndex].acknowledged = true;
+    {/* Couldn't change the acknowledged value inside a single comment object without recreating the whole dataset and replacing it.
+    I'm sure there is a more elegent solution */}
+    setCommentsData(clonedData);
+  };
 
   return (
     <section className='comment-component'>
@@ -15,9 +24,7 @@ export function Comment(props) {
         <div className="comment-foot">
           <p className="time-elapsed">{formatTimeAgo(date_time)}</p>
           <p className='separator'> | </p>
-          {/* If you re-render the comment by clicking on/off the bell the 'mark as seen' state is not preserved.
-          This is because there is no functionality to change the comments seenBool */}
-          { isSeen === false ? <p className='seen' onClick={() => {setIsSeen(true)}}>Mark as Seen</p> : null}
+          { acknowledged === false ? <p className='seen' onClick={() => {changeAcknowledged(commentPosition)}}>Mark as Seen</p> : null}
         </div>
       </div>
     </section>
@@ -25,8 +32,8 @@ export function Comment(props) {
 };
 
 {/* Function takes in the date_time and gives off the time since posting relative to how long ago it was */}
-function formatTimeAgo(props) {
-  const splitDate = props.replace(/[\/\s:]/g, ' ');
+function formatTimeAgo(date_time) {
+  const splitDate = date_time.replace(/[\/\s:]/g, ' ');
   const split = splitDate.split(' ');
   const formattedDate = `${split[2]}-${split[1]}-${split[0]}T${split[3]}:${split[4]}:00`;
   const dateTimeObj = new Date(formattedDate);
